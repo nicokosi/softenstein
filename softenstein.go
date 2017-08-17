36e6056e168b1ad1b631fd740039c994302030c3
Nicolas Kosinski <nicokosi@yahoo.com>
2017-08-17 12:53:29 +0200

    Build command triggers 2 threaded messages, simulating a long action
    Also:
    Add a reaction to indicate that command has succeeded.
    Remove "usage" response.

--- softenstein.go
+++ softenstein.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"regexp"
 
 	"github.com/nlopes/slack"
+	"time"
+	"fmt"
 )
 
 func main() {
@@ -18,23 +19,28 @@ func main() {
 	rtm := api.NewRTM()
 	go rtm.ManageConnection()
 
+	buildCommand := regexp.MustCompile("build (.*)")
+
 	for msg := range rtm.IncomingEvents {
 		switch ev := msg.Data.(type) {
 
 		case *slack.MessageEvent:
-			buildCommand := regexp.MustCompile("build (.*)")
 			buildArgs := buildCommand.FindStringSubmatch(ev.Text)
 			if len(buildArgs) > 1 {
-				rtm.SendMessage(
-					rtm.NewOutgoingMessage(
-						"Going to build\n> " + buildArgs[1],
-						ev.Channel))
-			} else {
-				rtm.SendMessage(
-					rtm.NewOutgoingMessage(
-						"Hmm, I don't know what to do. My supported command is\n> build <any message>",
-						ev.Channel))
+				msg := rtm.NewOutgoingMessage(
+					"Going to build\n> "+buildArgs[1],
+					ev.Channel)
+				msg.ThreadTimestamp = ev.Timestamp
+				rtm.SendMessage(msg)
+
+				time.Sleep(2 * time.Second)
+
+				msg2 := rtm.NewOutgoingMessage("Done!", ev.Channel)
+				msg2.ThreadTimestamp = msg.ThreadTimestamp
+				rtm.SendMessage(msg2)
+				rtm.AddReaction("white_check_mark", slack.NewRefToMessage(ev.Channel, ev.Timestamp))
 			}
+
 		case *slack.RTMError:
 			fmt.Printf("Error: %s\n", ev.Error())
 
